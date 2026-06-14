1803ca9e3fdba37657c9d8503d19fe0128349242
MuPrince <137839462@qq.com>
2026-06-14 23:54:47 +0800

    新增Java AI项目：A2A协议与MCP服务器实现

---
+++ Java Ai/demo01/prince-a2a-provider/src/main/java/com/prince/ai/a2a/provider/Application.java
@@ -0,0 +1,18 @@
+package com.prince.ai.a2a.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Description :  <br\>
+ *
+ * @author Mu.Prince <br\>
+ * @since 2026/5/14 22:42
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
